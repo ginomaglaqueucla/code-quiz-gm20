8706fd3a7ab499e2071bbc4932bd50880d8a2c54
ginomaglaque <ginomaglaque@gmail.com>
2020-08-28 21:58:31 -0700

    start quiz handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,38 @@
 var buttonEl =  document.querySelector("#start-quiz");
+// var divButtonEl = document.querySelector("#btn");
 var h1El = document.querySelector("#question");
+var choiceContainerEl = document.querySelector(".btn");
+
+var questionArray = [
+    {
+    question: "Which of the following styles a web application?",
+    choices: ["HTML", "CSS", "JavaScript", "Potato"],
+    answer: "CSS"
+    }
+];
 var startQuizHandler = function() {
-    console.log("button was");
-    h1El.textContent = "TEST TEST";
+    // create choice buttons
+    var choiceButton1El = buttonEl;
+    var choiceButton2El = document.createElement("button");
+    var choiceButton3El = document.createElement("button");
+    var choiceButton4El = document.createElement("button");
+
+    for (var i = 0; i < questionArray.length; i++) {
+        // Display question
+        h1El.textContent = questionArray[i].question;
+
+        choiceButton1El.textContent = questionArray[i].choices[0];
+        choiceContainerEl.appendChild(choiceButton1El);
+
+        choiceButton2El.textContent = questionArray[i].choices[1];
+        choiceContainerEl.appendChild(choiceButton2El);
+
+        choiceButton3El.textContent = questionArray[i].choices[2];
+        choiceContainerEl.appendChild(choiceButton3El);
+
+        choiceButton4El.textContent = questionArray[i].choices[3];
+        choiceContainerEl.appendChild(choiceButton4El);
+    }
 
 };
 
