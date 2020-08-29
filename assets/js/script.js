27f54619755e73f7c41f75f0a101ab66ec9150f3
ginomaglaque <ginomaglaque@gmail.com>
2020-08-28 17:31:42 -0700

    dynamically changed h1 element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-window.alert("Hello");
+var buttonEl =  document.querySelector("#start-quiz");
+var h1El = document.querySelector("#question");
+var startQuizHandler = function() {
+    console.log("button was");
+    h1El.textContent = "TEST TEST";
+
+};
+
+
+
+
+
+
+
+buttonEl.addEventListener("click", startQuizHandler);
