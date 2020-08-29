dc783fa31ed9955697556cce71b4f6c4362fb26c
ginomaglaque <ginomaglaque@gmail.com>
2020-08-28 22:52:31 -0700

    created transitional flow question to question

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,38 +8,80 @@ var questionArray = [
     question: "Which of the following styles a web application?",
     choices: ["HTML", "CSS", "JavaScript", "Potato"],
     answer: "CSS"
+    },
+    {
+    question: "What is the syntax for printing to console?",
+    choices: ["window.alert()", "printToConsole()", "console.log()", "Tomato"],
+    answer: "console.log()"
     }
 ];
+
+var choicesButtonArray = [];
+
+var questionCounter = 0;
+var startQuizFlag = false;
 var startQuizHandler = function() {
+    // remove start quiz button
+    buttonEl.remove();
     // create choice buttons
-    var choiceButton1El = buttonEl;
+    var choiceButton1El = document.createElement("button");;
     var choiceButton2El = document.createElement("button");
     var choiceButton3El = document.createElement("button");
     var choiceButton4El = document.createElement("button");
 
-    for (var i = 0; i < questionArray.length; i++) {
-        // Display question
-        h1El.textContent = questionArray[i].question;
-
-        choiceButton1El.textContent = questionArray[i].choices[0];
-        choiceContainerEl.appendChild(choiceButton1El);
-
-        choiceButton2El.textContent = questionArray[i].choices[1];
-        choiceContainerEl.appendChild(choiceButton2El);
-
-        choiceButton3El.textContent = questionArray[i].choices[2];
-        choiceContainerEl.appendChild(choiceButton3El);
-
-        choiceButton4El.textContent = questionArray[i].choices[3];
-        choiceContainerEl.appendChild(choiceButton4El);
-    }
-
+    choicesButtonArray = [
+        choiceButton1El, choiceButton2El, choiceButton3El, choiceButton4El
+    ];
+    displayQuestion(choicesButtonArray);    
 };
 
+var displayQuestion = function(choicesButtonArray) {
+    // Display question
+    h1El.textContent = questionArray[questionCounter].question;
 
+    choicesButtonArray[0].textContent = questionArray[questionCounter].choices[0];
+    choiceContainerEl.appendChild(choicesButtonArray[0]);
 
+    choicesButtonArray[1].textContent = questionArray[questionCounter].choices[1];
+    choiceContainerEl.appendChild(choicesButtonArray[1]);
+
+    choicesButtonArray[2].textContent = questionArray[questionCounter].choices[2];
+    choiceContainerEl.appendChild(choicesButtonArray[2]);
+
+    choicesButtonArray[3].textContent = questionArray[questionCounter].choices[3];
+    choiceContainerEl.appendChild(choicesButtonArray[3]);
+};
+
+var checkAnswerHandler = function(event) {
+
+    // grab user answer from button click
+    var userAnswer = event.target;
+
+    // correct answer
+    var correctAnswer = questionArray[questionCounter].answer;
+
+    // check if answer is correct
+    if (userAnswer.textContent === correctAnswer){
+        console.log("Good");
+    }
+    else {
+        console.log("BAD");
+    }
+
+    // Will increment counter after first question
+    if (!startQuizFlag) {
+        startQuizFlag = true;
+    } else {
+        questionCounter++;
+    }
+
+    displayQuestion(choicesButtonArray);
+
+    
+ }
 
 
 
 
 buttonEl.addEventListener("click", startQuizHandler);
+choiceContainerEl.addEventListener("click", checkAnswerHandler);
